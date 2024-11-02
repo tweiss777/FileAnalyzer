97689b81c8c26bc1b765b9354a9c5bf38b4e3022
Tal Weiss <Tweiss747@gmail.com>
2024-11-02 14:55:35 -0400

    Implemented basic results screen

--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { Ref, ref } from 'vue';
 import FileUpload from '../components/FileUpload.vue';
+import Results from '../components/Results.vue';
 const file = ref();
+const isLoading: Ref<boolean> = ref<boolean>(false);
 function setFile(doc: any)
 {
     file.value = doc
@@ -14,5 +16,6 @@ function upload(){
   <div>
     <h1 class="text-3xl text-black-500">Upload File</h1>
         <FileUpload @set-fle="setFile" @upload="upload"/>
+        <Results :loading="true"/>
   </div>
 </template>
